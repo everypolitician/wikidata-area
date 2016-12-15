7bb6e1c589fe6c3443b324a108dd9956b9ddd450
Tony Bowden <tony@tmtm.com>
2016-12-15 11:39:43 +0000

    Use nested module/class definitions

--- test/wikidata/area_test.rb
+++ test/wikidata/area_test.rb
@@ -1,17 +1,19 @@
 require 'test_helper'
 
-class Wikidata::AreaTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Wikidata::Area::VERSION
-  end
+module Wikidata
+  class AreaTest < Minitest::Test
+    def test_that_it_has_a_version_number
+      refute_nil ::Wikidata::Area::VERSION
+    end
 
-  # TODO: don't test against live data!
-  def test_live_data
-    data = Wikidata::Areas.new(ids: %w(Q3402163 Q3025278)).data
+    # TODO: don't test against live data!
+    def test_live_data
+      data = Wikidata::Areas.new(ids: %w(Q3402163 Q3025278)).data
 
-    assert_equal 2, data.count
-    kesklinn = data.find { |a| a[:id] == 'Q3025278' }
-    assert_includes kesklinn['name__en'], 'Kesklinn'
-    assert_includes kesklinn['name__et'], 'Valimisringkond'
+      assert_equal 2, data.count
+      kesklinn = data.find { |a| a[:id] == 'Q3025278' }
+      assert_includes kesklinn['name__en'], 'Kesklinn'
+      assert_includes kesklinn['name__et'], 'Valimisringkond'
+    end
   end
 end
